bfd33606803e50da7d59c6513b13f9afd0c83489
charul97 <charulagrawal97@gmail.com>
2017-08-24 03:11:11 +0530

    abc.py --entered abc.py -- file changed abcd.py --file changed
    Fixes https://google.com

---
+++ hr1.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main(){
+/*	int num;
+	int q=2;
+    int n=5;
+    vector<int> p[q];
+        for(int i = 0; i < q; i++){
+	   for(int j=0; j<n; j++){	
+           	cin >> num;
+	        p[i].push_back(num);
+        }
+     } */
+     
+     
+   float f=float(5)/2;
+   cout<<f;
+     return 0;
+ }    
